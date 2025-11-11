568e52a5ca18c101fbba408c5b2ab4fef636d4f5
Mayakruha <49606586+Mayakruha@users.noreply.github.com>
2025-11-11 21:44:54 +0100

    Delete Example/NodeToSurf.py

--- Example/NodeToSurf.py
+++
@@ -1,6 +0,0 @@
-from FEMtoolkit import import_abq
-model=import_abq('Sec_410_orig.inp')
-model.NodeIntoSurf('Outer')
-mesh=model.ReNumb()
-mesh.NSets={}
-mesh.export_abq('D://VTP//CCM//3DCCM//Sec_410.inp')
